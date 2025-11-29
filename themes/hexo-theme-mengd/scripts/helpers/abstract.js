daf9743440b256359c6ba61a8abfd4be34bf5090
Kowsar Hossain <kowsar9989@gmail.com>
2025-11-29 13:22:36 +0600

    hide noprovew from excerpt

--- themes/hexo-theme-mengd/scripts/helpers/abstract.js
+++ themes/hexo-theme-mengd/scripts/helpers/abstract.js
@@ -5,11 +5,17 @@
 
 const { stripHTML, truncate } = require('hexo-util')
 
+const removeNoPreviewBlocks = (input = '') =>
+  input.replace(/<!--\s*no_preview:start\s*-->[\s\S]*?<!--\s*no_preview:end\s*-->/gi, '')
+
 hexo.extend.helper.register('getAbstract', function (post, length = 200) {
   let abstract =
     post.abstract ||
     // Remove extra spaces and line breaks, as well as the line numbers of the code blocks
     post.content.replace(/\<td class=\"gutter\">\<pre>(.*?)\<\/pre>\<\/td>/gim, '').replace(/[\n]|[\r\n]/gim, '')
+
+  // Remove no_preview sections before stripping tags
+  abstract = removeNoPreviewBlocks(abstract)
 
   // Remove html tags
   abstract = stripHTML(abstract)
